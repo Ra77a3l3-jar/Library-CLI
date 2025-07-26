d56f07dc7fdfd12207d3d082befb2626f393f019
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-07-26 20:48:20 +0200

    i added the struct that i will be using in the library
    Library and Book for controlling the library and books, while Student
    and Student_System to controll the students and the books they borrowed

--- library_system.c
+++ library_system.c
@@ -3,6 +3,36 @@
 #include <ctype.h>
 #include <string.h>
 
+typedef struct {
+    char *title;
+    char **authors;        // Dynamic array of author name strings
+    int author_count;
+    int year;
+    int pages;
+    int is_available;      // 1 if available, 0 if borrowed
+    char *borrowed_by;     // Student name who borrowed it (NULL if available)
+} Book;
+
+typedef struct {
+    int student_id;
+    char *name;
+    char **borrowed_books; // Dynamic array of borrowed book titles
+    int borrowed_count;
+    int max_books;         // Maximum books this student can borrow (default 3)
+} Student;
+
+typedef struct {
+    Book *books;           // Dynamic array of books
+    int book_count;
+    int capacity;          // Current array capacity
+} Library;
+
+typedef struct {
+    Student *students;     // Dynamic array of students
+    int student_count;
+    int student_capacity;
+} StudentSystem;
+
 int main(void) {
 
     return 0;
