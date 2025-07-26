2c75252cee7c5b2e66b291e7a8cc672c78b2f2ec
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-07-26 20:50:06 +0200

    i just created a skech of the features that i would like to have in the Library

--- library_system.c
+++ library_system.c
@@ -33,6 +33,35 @@ typedef struct {
     int student_capacity;
 } StudentSystem;
 
+/* ================ FUNCTION DECLARATIONS ================== */
+
+// Library Management Functions
+Library* create_library(int initial_capacity);
+int add_book(Library *lib);
+void display_all_books(Library *lib);
+int search_books(Library *lib);
+int remove_book(Library *lib);
+void display_statistics(Library *lib);
+void cleanup_library(Library *lib);
+
+// Helper Functions
+Book* resize_library_if_needed(Library *lib);
+void cleanup_book(Book *book);
+int case_insensitive_search(const char *haystack, const char *needle);
+
+// Student Management Functions (TODO: Implement these)
+StudentSystem* create_student_system(int initial_capacity);
+int add_student(StudentSystem *sys);
+void display_all_students(StudentSystem *sys);
+int borrow_book(Library *lib, StudentSystem *sys);
+int return_book(Library *lib, StudentSystem *sys);
+void display_student_books(StudentSystem *sys);
+void display_enhanced_statistics(Library *lib, StudentSystem *sys);
+void cleanup_student_system(StudentSystem *sys);
+void cleanup_student(Student *student);
+Student* find_student_by_name(StudentSystem *sys, const char *name);
+Book* find_book_by_title(Library *lib, const char *title);
+
 int main(void) {
 
     return 0;
