c17e6e609736040b678f87282f0a18ddfe644eb2
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-07-26 21:27:02 +0200

    i added the function that allows to add a book and all his Info

--- library_system.c
+++ library_system.c
@@ -62,6 +62,8 @@ void cleanup_student(Student *student);
 Student* find_student_by_name(StudentSystem *sys, const char *name);
 Book* find_book_by_title(Library *lib, const char *title);
 
+/* ============= FUNCTION IMPLEMENTATION ============= */
+
 Library* create_library(int initial_capacity) {
     Library *lib = malloc(sizeof(Library));
     if (lib == NULL) {
@@ -81,6 +83,73 @@ Library* create_library(int initial_capacity) {
     
     return lib;
 }
+
+int add_book(Library *lib) {
+    if(lib->book_count >= lib->capacity) {
+        int new_capacity = 0;
+        printf("The array is full you need to add more space: ");
+        scanf("%d", &new_capacity);
+        new_capacity += lib->capacity;
+
+        Book *new_books = realloc(lib->books, sizeof(Book) * new_capacity);
+        if(new_books == NULL) {
+            printf("Memory allocation failed\n");
+            return 0;
+        }
+        lib->books = new_books;
+        lib->capacity = new_capacity;
+    }
+
+    char temp_title[256];
+    printf("Enter the book title: ");
+    scanf(" %255[^\n]", temp_title);  
+    lib->books[lib->book_count].title = malloc(strlen(temp_title) + 1);
+    strcpy(lib->books[lib->book_count].title, temp_title);
+
+    // Get number of authors
+    int num_authors;
+    printf("How many authors? ");
+    scanf("%d", &num_authors);
+    printf("\n");
+
+    if(num_authors > 10) {
+        printf("Too many authors MAX 10: ");
+        scanf("%d", &num_authors);
+        printf("\n");
+    }
+
+    // Allocate array of char* pointers
+    lib->books[lib->book_count].authors = malloc(sizeof(char*) * num_authors);
+    lib->books[lib->book_count].author_count = num_authors;
+
+    // Now allocate each individual author name - FIXED to handle spaces
+    for (int i = 0; i < num_authors; i++) {
+        char temp_author[256];
+        printf("Enter author %d: ", i + 1);
+        scanf(" %255[^\n]", temp_author);
+    
+        lib->books[lib->book_count].authors[i] = malloc(strlen(temp_author) + 1);
+        strcpy(lib->books[lib->book_count].authors[i], temp_author);
+    }
+
+    // Get book's year
+    int temp_year = 0;
+    printf("Enter the book's year: ");
+    scanf("%d", &temp_year);
+    lib->books[lib->book_count].year = temp_year;
+
+    // Get book's pages
+    int temp_pages = 0;
+    printf("Enter the total number of pages: ");
+    scanf("%d", &temp_pages);
+    lib->books[lib->book_count].pages = temp_pages;
+
+    lib->book_count++;
+    
+    return 1;
+}
+
+/* =============== MAIN FUNCTION ============== */
 
 int main(void) {
     Library *library = NULL;
